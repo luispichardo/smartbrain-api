b861709c0fe1c5b6a48337e312672eb7f0552941
luispichardo <l1vv88@gmail.com>
2021-10-18 23:19:30 -0400

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1", //localhost
+    host: "postgresql-closed-13887", //localhost
     port: 5432,
     user: "postgres",
     password: "1234",
